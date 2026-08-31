dfd303e334085b843c1fc17f6cb19c871c11c317
Diana Brodovaya <diana.brodovaya@gmail.com>
2026-08-31 18:19:45 +0700

    Добавляет переменные в компонент (задание 1.35)

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="app_layout">
-        <app-layout-header/>
-        <slot/>
-    </div>
+  <div>
+    <app-header />
+    <slot />
+  </div>
 </template>
 
 <script setup>
