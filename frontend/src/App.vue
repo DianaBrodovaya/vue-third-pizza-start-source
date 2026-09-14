7aaf91c174c4aeb82152d96be3c60ba10c1ce6bc
Diana Brodovaya <diana.brodovaya@gmail.com>
2026-09-14 19:01:35 +0700

    Добавляет маршрутизацию и динамические лейауты (задание 3.14)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <app-layout>
-    <home-view />
+    <router-view />
   </app-layout>
 </template>
 
 <script setup>
-import { AppLayout } from "@/layouts";
-import { HomeView } from "@/views";
+import AppLayout from "@/layouts/AppLayout.vue";
 </script>
 
 <style lang="scss">
